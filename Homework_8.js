0c2305a4643cf26a8364dbb3528a32a35cfe1110
Alexey <alexey.mueller81@gmail.com>
2023-12-17 19:18:36 +0100

    Homework_8

--- Homework_8.js
+++ Homework_8.js
@@ -1,32 +1,48 @@
-/*ДЗ2 Сума вартості послуг
-
-Припустимо, у нас є об'єкт. Властивості об'єкта - назва послуги та її вартість:
-
-var services = {
-"стрижка": "60 грн",
-"гоління": "80 грн",
-"Миття голови": "100 грн"
-};
-"Послуги" можуть додаватися по ходу роботи:
-   services['Розбити скло'] = "200 грн";
-Умови виконання ДЗ
+/*
 створити метод price(), який обчислює та повертає загальну вартість "наданих послуг".
 створити метод minPrice() - який повертає мінімальний price
 створити метод maxPrice() - який повертає максимальний price */
-// let services = {
-//     haircut : 26,
-//     shaving: 15,
-//     manicure: 25,
-//     totalPrice: function () {
-//         console.log("General services price is" + " " + (services.haircut + services.shaving + services.manicure))
-//         console.log("The lowest price is" + " " + Math.min(this.haircut, this.shaving, this.manicure))
-//         console.log("The highest price is" + " " + Math.max(this.haircut, this.shaving, this.manicure))
-//
-// }
-// }
+let services = {
+    "haircut" : 26,
+    "shaving": 15,
+    "manicure": 25,
+ 
+};
+
+function totalPrice(services) {
+
+    let sum = 0;
+    for (let key of Object.values(services)) {
+      sum += key;
+    }
+  
+    return sum;
+  }
+  
+  function lowestPrice (services) {
+   let minPrice =0;
+    for (let key in services) {
+        minPrice = Math.min(key)
+    }
+  return minPrice
+  }
+    
+  console.log( totalPrice(services) ); 
+  console.log( lowestPrice(services) );
+
+
+
+
+/*     totalPrice: function () {
+          console.log("General services price is" + " " + (services.haircut + services.shaving + services.manicure))
+          console.log("The lowest price is" + " " + Math.min(this.haircut, this.shaving, this.manicure))
+          console.log("The highest price is" + " " + Math.max(this.haircut, this.shaving, this.manicure)) */
+
+
+
 // services.totalPrice()
 
-/*ДЗ1
+/*ДЗ1 
 
 Створіть об'єкт з щонайменше трьома властивостями (намагайтесь виявити фантазію).
 Напишіть метод getInfo(), який виводитиме всі властивості об'єкта у вигляді пар "ключ: значення".
@@ -39,7 +55,7 @@ obj.newProperty = 'Нова властивість!'; // Додали власт
 obj.getInfo(); // Виводить всі властивості, включаючи щойно додану.
 Умови виконання ДЗ
 */
-let house = {
+/*let house = {
     livingRoom: 22,
     bedroom1: 18,
     bedroom2: 18,
@@ -49,7 +65,7 @@ let house = {
         return "living room is " + (this.livingRoom) + " " + "bedroom one is" + " " + (this.bedroom1) + " " + "bedroom two is" + " " + (this.bedroom2)
             }
 }
-console.log (house.houseTotal())
+console.log (house.houseTotal())*/
 
 
 
